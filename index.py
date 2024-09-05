4fc6dfef88378ab5b43210a0e9b68918431d15b4
Case Howard-Moreno <cahoward-moreno@stu.lltc.edu>
2024-09-05 10:22:40 -0500

    Introduction done

--- index.py
+++ index.py
@@ -2,7 +2,13 @@ def main():
     print("itech190-introduction")
     print("My Name is ")
     print("Case")
-
+    print("Why am i intrested in computer science")
+    print("Im intrested in computer science because im always on computers and seeing how computers are contantly evloving")
+    print("What do i want to do after i graduate from LLTC?")
+    print("I havent decided on what i wanted to do to be honest")
+    print("One fun fact about me is")
+    print("I enjoy to play videos games and to play/watch basketball")
+    
 
 if __name__ == "__main__":
     main()
